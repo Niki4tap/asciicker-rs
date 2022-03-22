639ed8fdabbdee78bf83f4873e1fba7139a9c4b6
Niki4tap <rombiklol2@gmail.com>
2022-03-22 15:07:41 +0300

    Add bot example in the source

--- src/y6/mod.rs
+++ src/y6/mod.rs
@@ -6,7 +6,41 @@
 /// Bot module is supposed to provide highest level of abstraction and allow easy creation
 /// of bots.
 ///
-/// Look in `examples/` directory for examples.
+/// # Examples
+///
+/// ## Simple chat logger:
+///
+/// ```rust
+/// use asciicker_rs::callback;
+/// use asciicker_rs::macro_rules_attribute::apply;
+/// use asciicker_rs::y6::prelude::*;
+/// use std::sync::Arc;
+/// use tokio::sync::Mutex;
+///
+/// #[tokio::main]
+/// async fn main() {
+///     let mut bot = Bot::new("player", "ws://asciicker.com/ws/y6/", true);
+///     bot.on_talk(talk_callback);
+///     let (threads, _data) = match bot.run().await {
+///         Err(e) => panic!("Failed to run the bot: {:?}", e),
+///         Ok(stuff) => stuff,
+///     };
+///     println!("{:?}", threads.0.thread.await);
+/// }
+///
+/// #[apply(callback!)]
+/// pub async fn talk_callback(
+///     talk_brc: TalkBroadcast,
+///     _: Arc<Mutex<Player>>,
+///     _: Arc<Mutex<World>>,
+///     _: MessageSender,
+/// ) -> BotResult {
+///     println!("{:?}", talk_brc.str);
+///     Ok(())
+/// }
+/// ```
+///
+/// Look in `examples/` directory more for examples.
 #[cfg(feature = "bot")]
 pub mod bot;
 /// # Packets module
